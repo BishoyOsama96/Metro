cd2fcc706a0eb20e159f4ba84a97bac25d94fff7
khadija267 <khadiga267@gmail.com>
2020-11-23 00:41:45 +0200

    stations added lines added finding number of stations and the rout added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int u=0, v=0;
+        int u, v;
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter your Start");
         String start=sc.nextLine();
