a0b096e0cf71c1c58962e5f6592d6cee282066ae
Bishoy Osama <beshoyprototype@hotmail.com>
2020-11-25 16:45:15 +0200

    Ticket Done we replaced list by line in Station Class I made Some changes in Station Class and the main and deleted ticket Class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,44 +2,52 @@ package com.company;
 import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
 
-        int u, v;
-        byte t ;
-        ticket ticket=new ticket();
+    public static void main(String[] args) {
+        Station st=new Station();
+        int u, v,nostation,extime,wtime=4;
+        byte t = 0;
 
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter your Start");
         String start=sc.nextLine();
         System.out.println("Enter your Destination");
         String destination = sc.nextLine();
-        if(Station.line1.contains(start) && Station.line1.contains(destination))
+        if(st.line1.contains(start) && st.line1.contains(destination))
         {
-            u=Station.line1.indexOf(start);
-            v=Station.line1.indexOf(destination);
-            t=ticket.TicketPrice(start,destination);
-            System.out.println("your path contains of "+(Math.max(u+1,v+1)-Math.min(u,v))+" Stations");
-            System.out.println("your rout is "+Station.line1.subList(Math.min(u,v),Math.max(u+1,v+1)));
+            u=st.line1.indexOf(start);
+            v=st.line1.indexOf(destination);
+            t=st.ticket(Math.max(u,v)-Math.min(u,v));
+            nostation=v-u;
+            extime=wtime*nostation;
+            System.out.println("your path contains of "+(Math.max(u,v)-Math.min(u,v))+" Stations");
+            System.out.println("your rout is "+st.line1.subList(Math.min(u,v),Math.max(u,v+1)));
             System.out.println("your ticket will cost: "+t+" pounds");
+            System.out.println("expected time is "+extime+"m");
 
         }
 
-        else if(Station.line2.contains(start) && Station.line2.contains(destination)){
-        u=Station.line2.indexOf(start);
-        v=Station.line2.indexOf(destination);
-        t=ticket.TicketPrice(start,destination);
-        System.out.println("your path contains of "+(Math.max(u+1,v+1)-Math.min(u,v))+" Stations");
-        System.out.println("your rout is "+Station.line2.subList(Math.min(u,v),Math.max(u+1,v+1)));
+        else if(st.line2.contains(start) && st.line2.contains(destination)){
+        u=st.line2.indexOf(start);
+        v=st.line2.indexOf(destination);
+            t=st.ticket(Math.max(u,v)-Math.min(u,v));
+            nostation=v-u;
+            extime=wtime*nostation;
+        System.out.println("your path contains of "+(Math.max(u,v)-Math.min(u,v))+" Stations");
+        System.out.println("your rout is "+st.line2.subList(Math.min(u,v),Math.max(u,v+1)));
             System.out.println("your ticket will cost: "+t+" pounds");
-
+            System.out.println("expected time is "+extime+"m");
     }
-    else if(Station.line3.contains(start) && Station.line3.contains(destination)){
-            u=Station.line3.indexOf(start);
-            v=Station.line3.indexOf(destination);
-            t=ticket.TicketPrice(start,destination);
-            System.out.println("your path contains of "+(Math.max(u+1,v+1)-Math.min(u,v))+" Stations");
-            System.out.println("your rout is "+Station.line3.subList(Math.min(u,v),Math.max(u+1,v+1)));
+    else if(st.line3.contains(start) && st.line3.contains(destination)){
+            u=st.line3.indexOf(start);
+            v=st.line3.indexOf(destination);
+            t=st.ticket(Math.max(u,v)-Math.min(u,v));
+            nostation=v-u;
+            extime=wtime*nostation;
+            System.out.println("your path contains of "+(Math.max(u,v)-Math.min(u,v))+" Stations");
+            System.out.println("your rout is "+st.line3.subList(Math.min(u,v),Math.max(u,v+1)));
             System.out.println("your ticket will cost: "+t+" pounds");
+            System.out.println("expected time is "+extime+"m");
         }
     else
             System.out.println("There is no Metro line connects your start with your destination.");
--- src/com/company/Station.java
+++ src/com/company/Station.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Station {
-     static ArrayList<String> line1=new ArrayList<>(Arrays.asList("Helwan","Ain Helwan","Helwan University",
+ private byte price;
+     public ArrayList<String> line1=new ArrayList<>(Arrays.asList("Helwan","Ain Helwan","Helwan University",
             "Wadi Hof","Hadayek Helwan","El-Maasara","Tora El-Asmant",
             "Kozzika","Tora El-Balad","Sakanat El-Maadi","Maadi","Hadayek El-Maadi",
             "Dar El-Salam","El-Zahraa","Mar Girgis","El-Malek El-Saleh","Al-Sayeda Zeinab",
@@ -13,18 +14,31 @@ public class Station {
             "Hadayeq El-Zaitoun","Helmeyet El-Zaitoun","El-Matareyya","Ain Shams",
             "Ezbet El-Nakhl","El-Marg","New El-Marg"));
 
-    static ArrayList<String> line2=new ArrayList<>(Arrays.asList( "El-Mounib","Sakiat Mekky"," Omm El-Masryeen","Giza","Faisal","Cairo University "
+    public ArrayList<String> line2=new ArrayList<>(Arrays.asList( "El-Mounib","Sakiat Mekky"," Omm El-Masryeen","Giza","Faisal","Cairo University "
             ,"El Bohoth","Dokki","Opera","Sadat","Mohamed Naguib","Attaba",
             "Al Shohadaa","Masarra","Rod El-Farag","St. Teresa","Khalafawy","Mezallat",
             "Kolleyyet El-Zeraa","Shubra El-Kheima"
     ));
 
-   static ArrayList<String> line3=new ArrayList<>(Arrays.asList("Airport","Ahmed Galal","Adly Mansour","El Haykestep","Omar Ibn El-Khattab",
+   public ArrayList<String> line3=new ArrayList<>(Arrays.asList("Airport","Ahmed Galal","Adly Mansour","El Haykestep","Omar Ibn El-Khattab",
             "Qobaa","Hesham Barakat","El-Nozha","Nadi El-Shams","Alf Maskan","Heliopolis Square",
             "Haroun","Al-Ahram","Koleyet El-Banat","Stadium","Fair Zone","Abbassiya",
             "Abdou Pasha","El-Geish","Bab El-Shaaria","Attaba","Nasser","Maspero","Zamalek",
             "Kit Kat","Sudan","Imbaba","El-Bohy","El-Kawmeya Al-Arabiya","Ring Road","Rod El-Farag",
             "El-Tawfikeya","Wadi El-Nil","Gamaat El Dowal Al-Arabiya","Bulaq El-Dakroor","Cairo University"
     ));
+public  byte ticket(int Count){
 
+
+    if (Count >= 1 && Count <= 9) {
+        this.price= 5;
+    } else if (Count > 9 && Count <= 16) {
+        this.price = 7;
+    } else if (Count > 16 && Count <= 39) {
+        this.price = 10;
+    }
+
+    return this.price;
 }
+}
+
