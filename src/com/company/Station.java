93cc038983c880f67258e6e9f48fc81204936055
Bishoy Osama <beshoyprototype@hotmail.com>
2020-11-25 17:21:55 +0200

    we replaced list by line in Station Class I made Some changes in Station Class and the main and deleted ticket Class Done

--- src/com/company/Station.java
+++ src/com/company/Station.java
@@ -5,14 +5,10 @@ import java.util.Arrays;
 
 public class Station {
  private byte price;
-     public ArrayList<String> line1=new ArrayList<>(Arrays.asList("Helwan","Ain Helwan","Helwan University",
-            "Wadi Hof","Hadayek Helwan","El-Maasara","Tora El-Asmant",
-            "Kozzika","Tora El-Balad","Sakanat El-Maadi","Maadi","Hadayek El-Maadi",
-            "Dar El-Salam","El-Zahraa","Mar Girgis","El-Malek El-Saleh","Al-Sayeda Zeinab",
-            "Saad Zaghloul","Sadat","Nasser","Orabi","Al-Shohadaa","Ghamra","El-Demerdash",
-            "Manshiet El-Sadr","Kobri El-Qobba","Hammamat El-Qobba","Saray El-Qobba",
-            "Hadayeq El-Zaitoun","Helmeyet El-Zaitoun","El-Matareyya","Ain Shams",
-            "Ezbet El-Nakhl","El-Marg","New El-Marg"));
+     public ArrayList<String> line1=new ArrayList<>(Arrays.asList("New El-Marg","El-Marg","Ezbet El-Nakhl","ain shams","El-Matareyya","Helmeyet El-Zaitoun","Hadayeq El-Zaitoun","Saray El-Qobba","Hammamat El-Qobba","Kobri El-Qobba",
+        "Manshiet El-Sadr","El-Demerdash","ghamra","Al Shohadaa","urabi","naser","sadat","Saad Zaghloul","AlSayyeda Zeinab","El-Malek El-Saleh",
+        "Mar Girgis","El-Zahraa","Dar El-Salam","Hadayeq El-Maadi","Maadi","Thakanat El-Maadi","Thakanat El-Maad","Tora El-Balad","opera"
+        ,"dokki","El Behoos","Kozzika","Tora El-Asmant","Tora El-Asmant","El-Maasara","Hadayeq Helwan","Wadi Hof","Helwan University","Ain Helwan","Helwan"));
 
     public ArrayList<String> line2=new ArrayList<>(Arrays.asList( "El-Mounib","Sakiat Mekky"," Omm El-Masryeen","Giza","Faisal","Cairo University "
             ,"El Bohoth","Dokki","Opera","Sadat","Mohamed Naguib","Attaba",
@@ -28,8 +24,6 @@ public class Station {
             "El-Tawfikeya","Wadi El-Nil","Gamaat El Dowal Al-Arabiya","Bulaq El-Dakroor","Cairo University"
     ));
 public  byte ticket(int Count){
-
-
     if (Count >= 1 && Count <= 9) {
         this.price= 5;
     } else if (Count > 9 && Count <= 16) {
@@ -37,7 +31,6 @@ public  byte ticket(int Count){
     } else if (Count > 16 && Count <= 39) {
         this.price = 10;
     }
-
     return this.price;
 }
 }
