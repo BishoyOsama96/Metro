11a161895cfc5311578c47b927847b2bd045c35d
Bishoy Osama <beshoyprototype@hotmail.com>
2020-11-26 17:13:22 +0200

    new edit

--- src/com/company/Station.java
+++ src/com/company/Station.java
@@ -6,14 +6,13 @@ public class Station {
      public ArrayList<String> line1=new ArrayList<>(Arrays.asList(
              "New El-Marg","El-Marg","Ezbet El-Nakhl","Ain Shams",
              "El-Matareyya","Helmeyet El-Zaitoun","Hadayeq El-Zaitoun"
-             ,"Saray El-Qobba","Hammamat El-Qobba","Kobri El-Qobba",
-             "Manshiet El-Sadr","El-Demerdash","Ghamra","Al Shohadaa",
-             "Urabi","Naser","Sadat","Saad Zaghloul","AlSayyeda Zeinab",
-             "El-Malek El-Saleh", "Mar Girgis","El-Zahraa","Dar El-Salam"
-             ,"Hadayeq El-Maadi","Maadi","Thakanat El-Maadi","Thakanat El-Maad"
-             ,"Tora El-Balad","Kozzika","Tora El-Asmant","Tora El-Asmant",
-             "El-Maasara","Hadayeq Helwan","Wadi Hof","Helwan University",
-             "Ain Helwan","Helwan"));
+             ,"Saray El-Qobba","Hammamat ElQobba","Kobri El-Qobba",
+             "Manshiet El-Sadr","El-Demerdash","Ghamra","Al-Shohadaa",
+             "Orabi","Nasser","Sadat","Saad Zaghloul","Al-Sayeda Zeinab",
+             "El-Malek El-Saleh","Mar Girgis","El-Zahraa'","Dar El-Salam",
+             "Hadayek El-Maadi","Maadi","Sakanat El-Maadi","Tora El-Balad",
+             "Kozzika","Tora El-Asmant","El-Maasara","Hadayek Helwan",
+             "Wadi Hof","Helwan University","Ain Helwan","Helwan"));
     public ArrayList<String> line2=new ArrayList<>(Arrays.asList( "El-Mounib","Sakiat Mekky"," Omm El-Masryeen","Giza","Faisal","Cairo University "
             ,"El Bohoth","Dokki","Opera","Sadat","Mohamed Naguib","Attaba",
             "Al Shohadaa","Masarra","Rod El-Farag","St. Teresa","Khalafawy","Mezallat",
